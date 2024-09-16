d8adafe297a334fb5fca0a87f7ada1627851b459
dini_landy <dini0527197044@gmail.com>
2024-09-16 16:32:06 +0300

    Add signOut compnent

--- src/Api/User.api.tsx
+++ src/Api/User.api.tsx
@@ -1,1 +1,10 @@
 import axios from 'axios';
+import { User } from '../Model/User.model';
+import { log } from 'console';
+
+const Url = 'https://mira-cohen-back-qv53.onrender.com/api/User/'
+export const AddUser=(user:User)=>{
+     debugger
+     console.log(user);
+  return axios.post(`${Url}Add`, user);  
+}
--- src/Model/User.model.tsx
+++ src/Model/User.model.tsx
@@ -1,1 +1,16 @@
-export interface User {}
+import { Institution } from "./Institution.model";
+export interface User {
+    id: string;
+    name: string;
+    email: string;       
+    password: string;    
+    role?: UserRole;     
+    // institutions?: string;  
+}
+
+export enum UserRole {
+    Manager = 'Manager',
+    Client = 'Client'
+
+}
+
